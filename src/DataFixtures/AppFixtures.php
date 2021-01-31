77a4b009e517e523f253c02218fd960996696da1
Khelifa Yassine <prostam27@gmail.com>
2021-02-01 00:49:22 +0100

    Fixtures avec des referentiels

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,25 +5,56 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Post;
+use App\Entity\User;
+use App\Entity\Comment;
+
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadUser($manager);
+        $this->loadPost($manager);
+      //  $this->loadUser($manager);
 
-        for ($i=0; $i < 20; $i++) { 
+
+    }
+    /**
+     * Fct add Post
+     */
+    private function loadPost(ObjectManager $manager){
             $post = new Post();
-            $post->setTitle("My Title ".$i);
-            $post->setSlug("My-Title-".$i);
+            $post->setTitle("My Title ");
+            $post->setSlug("My-Title-");
             $post->setPublished(new \DateTime());
-            $post->setContent("My Title ".$i);
-            $post->setAutor("User ".$i);
-
+            $post->setContent("My Title ");
+            $user = $this->getReference("admin_user");
+            $post->setAutor($user);
             $manager->persist( $post );
-        }
+            $manager->flush();
 
+    }
+     /**
+     * Fct add User
+     */
+    private function loadUser(ObjectManager $manager){
+        $user = new User();
+        $user->setUsername("Ya27cine");
+        $user->setEmail("yacinemosta910@gmail.com");
+        $user->setName("Khelifa Yassine");
+        $user->setPassword("PassW@rd");
+
+        $this->setReference("admin_user", $user);
+
+        $manager->persist( $user );
         $manager->flush();
+
+    }
+
+    /**
+     * Fct add Comment
+     */
+    private function loadComment(ObjectManager $manager){
+      
     }
 }
